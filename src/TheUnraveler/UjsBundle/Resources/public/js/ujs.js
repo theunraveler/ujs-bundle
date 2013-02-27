e79687eb3def20a225696a6a986b1abdbec8e449
Jake Bell <jake@theunraveler.com>
2013-02-27 12:59:19 -0600

    Adding class to UJS form

--- src/TheUnraveler/UjsBundle/Resources/public/js/ujs.js
+++ src/TheUnraveler/UjsBundle/Resources/public/js/ujs.js
@@ -10,7 +10,7 @@
             }
 
             // Append and submit the form.
-            $('<form>', { action: element.attr('href'), method: 'POST', style: 'display: none;' })
+            $('<form>', { action: element.attr('href'), method: 'POST', style: 'display: none;', class: 'ujs-form' })
                 .append($('<input>', { type: 'hidden', name: '_method', value: element.data('method') }))
                 .append($('<input>', { type: 'hidden', name: $('meta[name=_ujs_csrf_token_name]').attr('content'), value: $('meta[name=_ujs_csrf_token]').attr('content') }))
                 .insertAfter(element)
